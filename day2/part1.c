d0cca1c2fcb9abb9dc7e067bbd6a9824d31d2b26
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2023-12-10 18:17:19 +0000

    Complete AOC day 2 part 1

--- day2/part1.c
+++ day2/part1.c
@@ -25,7 +25,6 @@ enum ProgramMode {
 enum TestResult {
     TEST_PASSED,
     TEST_FAILED,
-    TEST_ERROR,
 };
 
 
@@ -33,6 +32,9 @@ enum TestResult {
 enum ProgramMode parseargs(int *argc, char* argv[]);
 GAME parsegame(char *line);
 enum TestResult test();
+int readword(char **lineptr, char* word);
+int parseInt(char* character);
+int validgame(GAME game);
 
 
 int main(int argc, char* argv[]) {
@@ -40,10 +42,6 @@ int main(int argc, char* argv[]) {
 
     if (mode == TEST_MODE) {
         enum TestResult result = test();
-        if (result == TEST_ERROR) {
-            printf("Tests errored\n");
-            exit(1);
-        }
         if (result == TEST_FAILED) {
             printf("Tests failed.\n");
             exit(1);
@@ -60,15 +58,25 @@ int main(int argc, char* argv[]) {
     }
 
     char line[MAX_LINE];
+    int isvalidgame;
+    GAME game;
+    int sum = 0;
     FILE *infile = fopen("input.txt", "r");
 
-    fgets(line, MAX_LINE, infile);
-    GAME game = parsegame(line);
-    printf("ID: %d\n", game.id);
-    printf("red: %d\n", game.red);
-    printf("green: %d\n", game.green);
-    printf("blue: %d\n", game.blue);
 
+    while (fgets(line, MAX_LINE, infile) != NULL) {
+        game = parsegame(line);
+        // printf("ID: %d\n", game.id);
+        // printf("red: %d\n", game.red);
+        // printf("green: %d\n", game.green);
+        // printf("blue: %d\n", game.blue);
+
+        isvalidgame = validgame(game);
+        if (isvalidgame > 0) {
+            sum += game.id;
+        };
+    }
+    printf("%d\n", sum);
 }
 
 
@@ -84,66 +92,305 @@ enum ProgramMode parseargs(int *argc, char* argv[]) {
     return INVALID_MODE;
 }
 
-// TODO: Remove this once line is used
-#pragma GCC diagnostic ignored "-Wunused-parameter"
+int validgame(GAME game) {
+    if (
+        game.red <= 12 &&
+        game.green <= 13 &&
+        game.blue <= 14
+    ) {
+        return game.id;
+    }
+    return -1;
+}
+
 GAME parsegame(char *line) {
     GAME game;
 
-    game.id = 1;
-    game.red = 1;
-    game.green = 1;
-    game.blue = 1;
+    game.id = 0;
+    game.red = 0;
+    game.green = 0;
+    game.blue = 0;
+
+    int result;
+    char word[MAX_WORD];
+    char word1[MAX_WORD];
+    char word2[MAX_WORD];
+    int red;
+    int green;
+    int blue;
+
+    // Possibly unnecessary
+    word[0] = '\0';
+    word1[0] = '\0';
+    word2[0] = '\0';
+
+    while ((result = readword(&line, word)) != EOF) {
+        strcpy(word1, word2);
+        strcpy(word2, word);
+        // printf("word1: '%-5s', word2: '%-5s'\n", word1, word2);
+        if (strcmp(word1, "Game") == 0) {
+            game.id = parseInt(word2);
+        };
+        if (strcmp(word2, "red") == 0) {
+            red = parseInt(word1);
+            if (red >= game.red) {
+                game.red = red;
+            }
+        };
+        if (strcmp(word2, "green") == 0) {
+            green = parseInt(word1);
+            if (green >= game.green) {
+                game.green = green;
+            }
+        };
+        if (strcmp(word2, "blue") == 0) {
+            blue = parseInt(word1);
+            if (blue >= game.blue) {
+                game.blue = blue;
+            }
+        };
+    }
 
     return game;
 }
 
 
-void readword(char **lineptr, char* word) {
-    char* line = *lineptr;
-
-    // Remove irrelevant leading characters
-    while (!isalpha(*line) && !isdigit(*line)) {
-        (line)++;
+void stripleading(char **lineptr) {
+    while (
+        !isalpha(**lineptr) &&
+        !isdigit(**lineptr) &&
+        **lineptr != '\0' && 
+        **lineptr != EOF
+    ) {
+        (*lineptr)++;
     }
-
-    // Read only alphabet and numeric characters
-    while (isalpha(*line) || isdigit(*line)) {
-        *word = *line;
-        line++; word++;
-    }
-    *word = '\0';
-
-    *lineptr = line;  // Update the original line pointer
 }
 
 
-enum TestResult test() {
+int parseIntChar(char character) {
+    /* 
+    Turn single character digit into an int
+    E.g: '8' becomes 8, '5' becomes 5.
+     */
+    return character - '0';
+}
+
+
+int parseInt(char* string) {
+    /* 
+    Turn string of character digits into an int 
+    E.g: '83' becomes 83, '50' becomes 50.
+     */
+    int result = -1;
+
+    for (unsigned long i=0; i<strlen(string); i++) {
+        if (result == -1) {
+            result = 0;
+        }
+        result = (result * 10) + parseIntChar(string[i]);
+    }
+    
+    return result;
+}
+
+
+int readword(char **lineptr, char* word) {
+    // printf("Reading word, strlen(): %zu\n", strlen(*lineptr));
+    if (strlen(*lineptr) < 1) {
+        return EOF;
+    }
+    stripleading(&*lineptr);
+
+    // Read only alphabetic and numeric characters
+    while (isalpha(**lineptr) || isdigit(**lineptr)) {
+        *word = **lineptr;
+        (*lineptr)++; word++;
+    }
+    *word = '\0';
+
+    return 1;  // success
+}
+
+
+enum TestResult test_readword() {
     char word[MAX_WORD];
     char *line = "Game 1: Some Example Input\0";
+    int result;
 
-    readword(&line, word);
-    if (strcmp(word, "Game") != 0)  {
+    result = readword(&line, word);
+    if (result == EOF || strcmp(word, "Game") != 0)  {
         printf("Test 1 failed\n");
-        printf("Expected '%s' to equal %s\n", word, "Game");
+        printf("Expected '%s' to equal '%s'\n", word, "Game");
         return TEST_FAILED;
     }
     printf("Test 1 passed\n");
 
-    readword(&line, word);
-    if (strcmp(word, "1") != 0)  {
+    result = readword(&line, word);
+    if (result == EOF || strcmp(word, "1") != 0)  {
         printf("Test 2 failed\n");
-        printf("Expected '%s' to equal %s\n", word, "1");
+        printf("Expected '%s' to equal '%s'\n", word, "1");
         return TEST_FAILED;
     }
     printf("Test 2 passed\n");
 
-    readword(&line, word);
-    if (strcmp(word, "Some") != 0)  {
+    result = readword(&line, word);
+    if (result == EOF || strcmp(word, "Some") != 0)  {
         printf("Test 3 failed\n");
-        printf("Expected '%s' to equal %s\n", word, "Some");
+        printf("Expected '%s' to equal '%s'\n", word, "Some");
         return TEST_FAILED;
     }
     printf("Test 3 passed\n");
+
+    result = readword(&line, word);
+    if (result == EOF || strcmp(word, "Example") != 0)  {
+        printf("Test 4 failed\n");
+        printf("Expected '%s' to equal '%s'\n", word, "Example");
+        return TEST_FAILED;
+    }
+    printf("Test 4 passed\n");
+
+    result = readword(&line, word);
+    if (result == EOF || strcmp(word, "Input") != 0)  {
+        printf("Test 5 failed\n");
+        printf("Expected '%s' to equal '%s'\n", word, "Input");
+        return TEST_FAILED;
+    }
+    printf("Test 5 passed\n");
+
+    result = readword(&line, word);
+    if (result != EOF)  {
+        printf("Test 6 failed\n");
+        printf("Expected readword to return EOF but got result: %d", result);
+        return TEST_FAILED;
+    }
+    printf("Test 6 passed\n");
+
+    return TEST_PASSED;
+}
+
+
+enum TestResult test_parseintchar() {
+    int result;
+    int expected;
+
+    result = parseIntChar('0');
+    expected = 0;
+    if (result != expected)  {
+        printf("Test 1 failed\n");
+        printf("Expected parseintchar('0') '%d' to equal '%d'\n", result, expected);
+        return TEST_FAILED;
+    }
+    printf("Test 1 passed\n");
+
+    result = parseIntChar('1');
+    expected = 1;
+    if (result != expected)  {
+        printf("Test 2 failed\n");
+        printf("Expected parseint('1') '%d' to equal '%d'\n", result, expected);
+        return TEST_FAILED;
+    }
+    printf("Test 2 passed\n");
+
+    return TEST_PASSED;
+}
+
+
+enum TestResult test_parseint() {
+    int result;
+    int expected;
+
+    result = parseInt("1");
+    expected = 1;
+    if (result != expected)  {
+        printf("Test 1 failed\n");
+        printf("Expected parseint(\"1\") '%d' to equal '%d'\n", result, expected);
+        return TEST_FAILED;
+    }
+    printf("Test 1 passed\n");
+
+    result = parseInt("12");
+    expected = 12;
+    if (result != expected)  {
+        printf("Test 2 failed\n");
+        printf("Expected parseint(\"12\") '%d' to equal '%d'\n", result, expected);
+        return TEST_FAILED;
+    }
+    printf("Test 2 passed\n");
+
+    return TEST_PASSED;
+}
+
+
+enum TestResult test_parsegame() {
+    int expected;
+    char line[] = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+    GAME game = parsegame(line);
+
+    expected = 1;
+    if (game.id != expected) {
+        printf("Test 1 failed\n");
+        printf("Expected game.id %d to equal %d\n", game.id, expected);
+        return TEST_FAILED;
+    }
+
+    expected = 4;
+    if (game.red != expected) {
+        printf("Test 1 failed\n");
+        printf("Expected game.red %d to equal %d\n", game.red, expected);
+        return TEST_FAILED;
+    }
+
+    expected = 2;
+    if (game.green != expected) {
+        printf("Test 1 failed\n");
+        printf("Expected game.green %d to equal %d\n", game.green, expected);
+        return TEST_FAILED;
+    }
+
+    expected = 6;
+    if (game.blue != expected) {
+        printf("Test 1 failed\n");
+        printf("Expected game.blue %d to equal %d\n", game.blue, expected);
+        return TEST_FAILED;
+    }
+
+    printf("Test 1 passed\n");
+
+    return TEST_PASSED;
+}
+
+
+enum TestResult test() {
+    enum TestResult result;
+    printf("\nRunning tests\n\n");
+
+    printf("- test_readword\n");
+    result = test_readword();
+    if (result == TEST_FAILED) {
+        return TEST_FAILED;
+    };
+    printf("\n");
+
+    printf("- test_parseintchar\n");
+    result = test_parseintchar();
+    if (result == TEST_FAILED) {
+        return TEST_FAILED;
+    };
+    printf("\n");
+
+    printf("- test_parseint\n");
+    result = test_parseint();
+    if (result == TEST_FAILED) {
+        return TEST_FAILED;
+    };
+    printf("\n");
+
+    printf("- test_parsegame\n");
+    result = test_parsegame();
+    if (result == TEST_FAILED) {
+        return TEST_FAILED;
+    };
+    printf("\n");
 
     return TEST_PASSED;
 }
