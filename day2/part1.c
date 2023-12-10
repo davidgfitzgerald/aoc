2654f0c8685494d78660a2b3acf77bd88485aa6b
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2023-12-10 20:32:11 +0000

    Refactor day 2 part 1: Add SET_MAX macro and format file

--- day2/part1.c
+++ day2/part1.c
@@ -2,27 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
 #include "part1.h"
 #include "test_part1.h"
 
+#define SET_MAX(COLOUR)                \
+    if (strcmp(word2, #COLOUR) == 0) { \
+        int value = parseInt(word1);   \
+        if (value >= game.COLOUR) {    \
+            game.COLOUR = value;       \
+        }                              \
+    };
 
-int main(int argc, char* argv[]) {
+
+int main(int argc, char *argv[])
+{
     enum ProgramMode mode = parseargs(&argc, argv);
 
-    if (mode == TEST_MODE) {
+    if (mode == TEST_MODE)
+    {
         enum TestResult result = test();
-        if (result == TEST_FAILED) {
+        if (result == TEST_FAILED)
+        {
             printf("Tests failed.\n");
             exit(1);
         }
-        if (result == TEST_PASSED) {
+        if (result == TEST_PASSED)
+        {
             printf("All tests passed.\n");
             exit(0);
         }
     }
 
-    if (mode != REGULAR_MODE) {
+    if (mode != REGULAR_MODE)
+    {
         printf("Error: Not running in regular mode");
         exit(1);
     }
@@ -33,135 +45,129 @@ int main(int argc, char* argv[]) {
     int sum = 0;
     FILE *infile = fopen("input.txt", "r");
 
-
-    while (fgets(line, MAX_LINE, infile) != NULL) {
+    while (fgets(line, MAX_LINE, infile) != NULL)
+    {
         game = parsegame(line);
 
         isvalidgame = validgame(game);
-        if (isvalidgame > 0) {
+        if (isvalidgame > 0)
+        {
             sum += game.id;
         };
     }
     printf("%d\n", sum);
 }
 
-
-enum ProgramMode parseargs(int *argc, char* argv[]) {
-    if (*argc == 1) {
+enum ProgramMode parseargs(int *argc, char *argv[])
+{
+    if (*argc == 1)
+    {
         return REGULAR_MODE;
     }
 
-    if (*argc == 2 && strcmp(argv[1], "--test") == 0) {
+    if (*argc == 2 && strcmp(argv[1], "--test") == 0)
+    {
         return TEST_MODE;
     }
 
     return INVALID_MODE;
 }
 
-int validgame(GAME game) {
+int validgame(GAME game)
+{
     if (
         game.red <= 12 &&
         game.green <= 13 &&
-        game.blue <= 14
-    ) {
+        game.blue <= 14)
+    {
         return game.id;
     }
     return -1;
 }
 
-GAME parsegame(char *line) {
+GAME parsegame(char *line)
+{
     GAME game = {.id = 0, .red = 0, .green = 0, .blue = 0};
 
     int result;
     char word[MAX_WORD];
     char word1[MAX_WORD];
     char word2[MAX_WORD];
-    int red;
-    int green;
-    int blue;
 
-    while ((result = readword(&line, word)) != EOF) {
+    while ((result = readword(&line, word)) != EOF)
+    {
         strcpy(word1, word2);
         strcpy(word2, word);
 
-        if (strcmp(word1, "Game") == 0) {
+        if (strcmp(word1, "Game") == 0)
+        {
             game.id = parseInt(word2);
         };
 
-        if (strcmp(word2, "red") == 0) {
-            red = parseInt(word1);
-            if (red >= game.red) {
-                game.red = red;
-            }
-        };
-        if (strcmp(word2, "green") == 0) {
-            green = parseInt(word1);
-            if (green >= game.green) {
-                game.green = green;
-            }
-        };
-        if (strcmp(word2, "blue") == 0) {
-            blue = parseInt(word1);
-            if (blue >= game.blue) {
-                game.blue = blue;
-            }
-        };
+        SET_MAX(red);
+        SET_MAX(green);
+        SET_MAX(blue);
     }
 
     return game;
 }
 
-
-void stripleading(char **lineptr) {
+void stripleading(char **lineptr)
+{
     while (
         !isalpha(**lineptr) &&
         !isdigit(**lineptr) &&
-        **lineptr != '\0' && 
-        **lineptr != EOF
-    ) {
+        **lineptr != '\0' &&
+        **lineptr != EOF)
+    {
         (*lineptr)++;
     }
 }
 
-
-int parseIntChar(char character) {
-    /* 
+int parseIntChar(char character)
+{
+    /*
     Turn single character digit into an int
     E.g: '8' becomes 8, '5' becomes 5.
      */
     return character - '0';
 }
 
-
-int parseInt(char* string) {
-    /* 
-    Turn string of character digits into an int 
+int parseInt(char *string)
+{
+    /*
+    Turn string of character digits into an int
     E.g: '83' becomes 83, '50' becomes 50.
      */
     int result = -1;
 
-    for (unsigned long i=0; i<strlen(string); i++) {
-        if (result == -1) {
+    for (unsigned long i = 0; i < strlen(string); i++)
+    {
+        if (result == -1)
+        {
             result = 0;
         }
         result = (result * 10) + parseIntChar(string[i]);
     }
-    
+
     return result;
 }
 
-
-int readword(char **lineptr, char* word) {
-    if (strlen(*lineptr) < 1) {
+int readword(char **lineptr, char *word)
+{
+    if (strlen(*lineptr) < 1)
+    {
         return EOF;
     }
     stripleading(&*lineptr);
 
-    while (isalpha(**lineptr) || isdigit(**lineptr)) {
+    while (isalpha(**lineptr) || isdigit(**lineptr))
+    {
         *word = **lineptr;
-        (*lineptr)++; word++;
+        (*lineptr)++;
+        word++;
     }
     *word = '\0';
 
-    return 1;  // success
+    return 1; // success
 }
